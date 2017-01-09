55510fe421c665afe83e6c0ddab413a1c58b04bd
v-afrafi <v-afrafi@microsoft.com>
2017-01-09 11:40:47 -0800

    resultset test updates

---
+++ src/test/java/com/microsoft/sqlserver/jdbc/resultset/ResultSetTest.java
@@ -0,0 +1,75 @@
+/*
+ * Microsoft JDBC Driver for SQL Server
+ * 
+ * Copyright(c) 2016 Microsoft Corporation All rights reserved.
+ * 
+ * This program is made available under the terms of the MIT License. See the LICENSE file in the project root for more information.
+ */
+package com.microsoft.sqlserver.jdbc.resultset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLFeatureNotSupportedException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.microsoft.sqlserver.testframework.AbstractTest;
+import com.microsoft.sqlserver.testframework.util.RandomUtil;
+
+@RunWith(JUnitPlatform.class)
+public class ResultSetTest extends AbstractTest {
+    private static final String tableName = "[" + RandomUtil.getIdentifier("StatementParam") + "]";
+
+    /**
+     * Tests proper exception for unsupported operation
+     * @throws Exception
+     */
+    @Test
+    public void testJdbc41ResultSetMethods() throws Exception {
+        Connection con = DriverManager.getConnection(connectionString);
+        Statement stmt = con.createStatement();
+        try {
+            stmt.executeUpdate("create table " + tableName + " (col1 int, col2 text, col3 int identity(1,1) primary key)");
+
+            stmt.executeUpdate("Insert into " + tableName + " values(0, 'hello')");
+
+            stmt.executeUpdate("Insert into " + tableName + " values(0, 'yo')");
+
+            ResultSet rs = stmt.executeQuery("select * from " + tableName);
+            rs.next();
+            // Both methods throw exceptions
+            try {
+
+                int col1 = rs.getObject(1, Integer.class);
+            }
+            catch (Exception e) {
+                // unsupported feature
+                assertEquals(e.getClass(), SQLFeatureNotSupportedException.class, "Verify exception type: " + e.getMessage());
+            }
+            try {
+                String col2 = rs.getObject("col2", String.class);
+            }
+            catch (Exception e) {
+                // unsupported feature
+                assertEquals(e.getClass(), SQLFeatureNotSupportedException.class, "Verify exception type: " + e.getMessage());
+            }
+            try {
+                stmt.executeUpdate("drop table " + tableName);
+            }
+            catch (Exception ex) {
+                fail(ex.toString());
+            }
+        }
+        finally {
+            stmt.close();
+            con.close();
+        }
+    }
+}
