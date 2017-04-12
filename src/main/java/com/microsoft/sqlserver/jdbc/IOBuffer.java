51097f27caa19dc1653232768b5b86dce7479506
Suraiya Hameed <v-suhame@microsoft.com>
2017-04-11 17:07:44 -0700

    Revert "update exceptions"
    This reverts commit 8dd8e4fdd9331b313a374f6fc078a7d41f51abda.

--- src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
@@ -3337,7 +3337,7 @@ final class TDSWriter {
         catch (ArithmeticException e) {
             MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_valueOutOfRange"));
             Object[] msgArgs = {SSType.DECIMAL};
-            throw new SQLServerException(form.format(msgArgs), e);
+            throw new SQLServerException(form.format(msgArgs), SQLState.DATA_EXCEPTION_DATETIME_FIELD_OVERFLOW, DriverError.NOT_SET, e);
         }
 
         int bLength;
@@ -4609,16 +4609,8 @@ final class TDSWriter {
                                 writeByte((byte) TDSWriter.BIGDECIMAL_MAX_LENGTH); // maximum length
                                 BigDecimal bdValue = new BigDecimal(currentColumnStringValue);
 
-                                try {
-                                    // setScale of all BigDecimal value based on metadata sent
-                                    bdValue = bdValue.setScale(columnPair.getValue().scale);
-                                }
-                                catch (ArithmeticException e) {
-                                    MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_valueOutOfRange"));
-                                    Object[] msgArgs = {SSType.DECIMAL};
-                                    throw new SQLServerException(form.format(msgArgs), e);
-                                }
-                                
+                                // setScale of all BigDecimal value based on metadata sent
+                                bdValue = bdValue.setScale(columnPair.getValue().scale);
                                 byte[] valueBytes = DDC.convertBigDecimalToBytes(bdValue, bdValue.scale());
 
                                 // 1-byte for sign and 16-byte for integer
