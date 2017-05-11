f9ea4ac4ffb0672e8a8ed107f7d869000ffb3a1d
v-nish <v-nisidh@microsoft.com>
2017-05-11 16:43:11 -0700

    Added parametrized test cases
    Added parametrized test cases

--- src/test/java/com/microsoft/sqlserver/jdbc/unit/TestSSLProtocol.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/unit/TestSSLProtocol.java
@@ -16,7 +16,9 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
@@ -25,7 +27,7 @@ import com.microsoft.sqlserver.jdbc.StringUtils;
 import com.microsoft.sqlserver.testframework.AbstractTest;
 
 /**
- * 
+ * This unit test case targeted to test new functionality of configuring SSLProtocol for connecting to SQL Server.  
  */
 @RunWith(JUnitPlatform.class)
 public class TestSSLProtocol extends AbstractTest {
@@ -34,53 +36,51 @@ public class TestSSLProtocol extends AbstractTest {
     Statement stmt = null;
 
     /**
-     * TODO: add @ParameterizedTest() with @ValueSource(strings={"TLS", "TLSv1.1"})
      * 
+     * @param sslProtocol
      * @throws Exception
      */
-    @Test
-    public void testConnectWithTLS1_2() throws Exception {
-        String url = connectionString + ";sslProtocol=TLSv1.2";
+    @DisplayName("TestForSupportedProtocols") 
+    @ParameterizedTest
+    @ValueSource(strings={"TLS","TLSv1.1","TLSv1.2"}) 
+    public void testSuportedProtocols(String sslProtocol) throws Exception {
+        String url = connectionString + ";sslProtocol=" + sslProtocol;
         con = DriverManager.getConnection(url);
         DatabaseMetaData dbmd = con.getMetaData();
         assertNotNull(dbmd);
         assertTrue(!StringUtils.isEmpty(dbmd.getDatabaseProductName()));
     }
 
-    @Test
-    public void testConnectWithTLS1_1() throws Exception {
-        String url = connectionString + ";sslProtocol=TLSv1.1";
-        con = DriverManager.getConnection(url);
-        DatabaseMetaData dbmd = con.getMetaData();
-        assertNotNull(dbmd);
-        assertTrue(!StringUtils.isEmpty(dbmd.getDatabaseProductName()));
-    }
-
-    @Test
-    public void testConnectWithSSL() throws Exception {
+    /**
+     * Connect with valid but not supported protocols
+     * @param sslProtocol
+     * @throws Exception
+     */
+    @DisplayName("TestForNotSupportedProtocols") 
+    @ParameterizedTest
+    @ValueSource(strings={"SSLv1","SSLv2","SSLv3", "SSLv2Hello","SSL"}) 
+    public void testWithUnSupportedProtocols(String sslProtocol) throws Exception {
         try {
-        String url = connectionString + ";sslProtocol=SSLv3";
+        String url = connectionString + ";sslProtocol=" + sslProtocol;
         con = DriverManager.getConnection(url);
-        assertFalse(true, "With Garbage Protocol you should not see this");
-        }catch(Exception e) {
+        assertFalse(true, "Any protocol other than TLS, TLSv1.1 & TLSv1.2 should throw Exception");
+        }catch(SQLServerException e) {
             assertTrue(true,"Should throw exception");
+            String errMsg = "SSL Protocol "+ sslProtocol +" is not valid. Supporting only TLS, TLSv1.1 & TLSv1.2.";
+            
+            assertTrue(errMsg.equals(e.getMessage()),"Message should be from SQL Server resources : " + e.getMessage());
         }
     }
 
     /**
-     * Wrong version number used.
+     * Test with wrong values.
+     * @param sslProtocol
      * @throws Exception
      */
-    @Test
-    public void testConnectWithWrongProtocol() throws Exception {
-        try {
-            String url = connectionString + ";sslProtocol=TLSv1.11";
-            con = DriverManager.getConnection(url);
-            assertFalse(true, "With Garbage Protocol you should not see this");
-        }
-        catch (SQLServerException e) {
-            assertTrue("SSL Protocol TLSv1.11 is not valid. Supporting only TLS, TLSv1.1 & TLSv1.2.".equals(e.getMessage()),"Message should be from SQL Server resources");
-        }
+    @DisplayName("TestForWrongValues") 
+    @ParameterizedTest
+    @ValueSource(strings={"SSLv1111","SSLv2222","SSLv3111", "SSLv2Hello1111","TLSv1.11","TLSv2.4","HTTPS"}) 
+    public void testConnectWithWrongProtocol(String sslProtocol) throws Exception {
+        testWithUnSupportedProtocols(sslProtocol); 
     }
-
 }
