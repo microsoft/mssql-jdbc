d6d7b6c6a74ebb61ea3ec18c4cdf8100d5846154
v-ahibr <v-ahibr@microsoft.com>
2017-06-26 15:13:20 -0700

    Makes sure we dont call getParameterEncryptionMetadata more than once

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
@@ -2562,7 +2562,7 @@ public class SQLServerPreparedStatement extends SQLServerStatement implements IS
 
             // Get the encryption metadata for the first batch only.
             if ((0 == numBatchesExecuted) && (Util.shouldHonorAEForParameters(stmtColumnEncriptionSetting, connection)) && (0 < batchParam.length)
-                    && !isInternalEncryptionQuery) {
+                    && !isInternalEncryptionQuery && !encryptionMetadataIsRetrieved) {
                 getParameterEncryptionMetadata(batchParam);
 
                 // fix an issue when inserting unicode into non-encrypted nchar column using setString() and AE is on on Connection
