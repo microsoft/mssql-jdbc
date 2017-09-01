c1ec1f6721720f5f8160833c1f2fafab8a3552db
Sehrope Sarkuni <sehrope@jackdb.com>
2017-09-01 07:16:08 -0400

    Refactor references to trust managers in tests

--- src/test/java/com/microsoft/sqlserver/jdbc/ssl/trustmanager/CustomTrustManagerTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/ssl/trustmanager/CustomTrustManagerTest.java
@@ -15,43 +15,39 @@ import com.microsoft.sqlserver.testframework.AbstractTest;
 public class CustomTrustManagerTest extends AbstractTest {
 
     /**
-     * Connect with a permissive Trust Manager that always accepts the X509Certificate chain offered to it. The test assumes PermissiveTrustManager
-     * class exists in the current package.
+     * Connect with a permissive Trust Manager that always accepts the X509Certificate chain offered to it.
      * 
      * @throws Exception
      */
     @Test
     public void testWithPermissiveX509TrustManager() throws Exception {
-        String url = connectionString + ";trustManagerClass=" + this.getClass().getPackage().getName() + ".PermissiveTrustManager" + ";encrypt=true;";
+        String url = connectionString + ";trustManagerClass=" + PermissiveTrustManager.class.getName() + ";encrypt=true;";
         SQLServerConnection con = (SQLServerConnection) DriverManager.getConnection(url);
         assertTrue(con != null);
     }
 
     /**
-     * Connect with a Trust Manager that requires trustManagerConstructorArg. The test assumes TrustManagerWithConstructorArg class exists in the
-     * current package.
+     * Connect with a Trust Manager that requires trustManagerConstructorArg.
      * 
      * @throws Exception
      */
     @Test
     public void testWithTrustManagerConstructorArg() throws Exception {
-        String url = connectionString + ";trustManagerClass=" + this.getClass().getPackage().getName() + ".TrustManagerWithConstructorArg"
+        String url = connectionString + ";trustManagerClass=" + TrustManagerWithConstructorArg.class.getName()
                 + ";trustManagerConstructorArg=dummyString;" + ";encrypt=true;";
         SQLServerConnection con = (SQLServerConnection) DriverManager.getConnection(url);
         assertTrue(con != null);
     }
 
     /**
-     * Test with a custom Trust Manager that does not implement X509TrustManager The test assumes InvalidTrustManager class exists in the current
-     * package.
+     * Test with a custom Trust Manager that does not implement X509TrustManager.
      * 
      * @throws Exception
      */
     @Test
     public void testWithInvalidTrustManager() throws Exception {
         try {
-            String url = connectionString + ";trustManagerClass=" + this.getClass().getPackage().getName() + ".InvalidTrustManager"
-                    + ";encrypt=true;";
+            String url = connectionString + ";trustManagerClass=" + InvalidTrustManager.class.getName() + ";encrypt=true;";
             SQLServerConnection con = (SQLServerConnection) DriverManager.getConnection(url);
             assertTrue(con == null);
         }
