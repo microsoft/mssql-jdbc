e601b81dc84a7b17957d16510c3babaebe5d64b9
Shawn Sun <v-xiangs@microsoft.com>
2017-12-12 15:32:40 -0800

    use javax.security.auth.kerberos instead of sun.security.krb5.Credentials

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
@@ -391,6 +391,5 @@ public final class SQLServerResource extends ListResourceBundle {
 				{"R_invalidDataTypeSupportForSQLVariant", "Unexpected TDS type ' '{0}' ' in SQL_VARIANT."},
 				{"R_sslProtocolPropertyDescription", "SSL protocol label from TLS, TLSv1, TLSv1.1 & TLSv1.2. The default is TLS."},
 				{"R_invalidSSLProtocol", "SSL Protocol {0} label is not valid. Only TLS, TLSv1, TLSv1.1 & TLSv1.2 are supported."},
-				{"R_AADIntegratedTGTNotFound", "Kerberos ticket-granting ticket (TGT) cannot be found."},
     };
 }
