5db2b836a54fc77c0fa28c441ff0b1c9697ccaf1
rene-ye <rene-ye@users.noreply.github.com>
2018-05-01 14:40:32 -0700

    Added MIT License comment

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerLob.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerLob.java
@@ -1,3 +1,10 @@
+/*
+ * Microsoft JDBC Driver for SQL Server
+ * 
+ * Copyright(c) Microsoft Corporation All rights reserved.
+ * 
+ * This program is made available under the terms of the MIT License. See the LICENSE file in the project root for more information.
+ */
 package com.microsoft.sqlserver.jdbc;
 
 import java.sql.SQLException;
