0224a03e9a09585b630ae3d9f50705d3ae7bb122
Cheena Malhotra <v-chmalh@microsoft.com>
2018-06-05 15:12:32 -0700

    Formatted code + dropTable method called from Utils

--- src/test/java/com/microsoft/sqlserver/jdbc/resultset/DataClassificationTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/resultset/DataClassificationTest.java
@@ -12,48 +12,51 @@ import org.junit.runner.RunWith;
 import com.microsoft.sqlserver.jdbc.SQLServerResultSet;
 import com.microsoft.sqlserver.jdbc.dataclassification.SensitivityProperty;
 import com.microsoft.sqlserver.testframework.AbstractTest;
+import com.microsoft.sqlserver.testframework.Utils;
 import com.microsoft.sqlserver.testframework.util.RandomUtil;
 
 @RunWith(JUnitPlatform.class)
 public class DataClassificationTest extends AbstractTest {
     private static final String tableName = "[" + RandomUtil.getIdentifier("DataClassification") + "]";
-    
+
     /**
      * Tests data classification metadata information from SQL Server
-     * @throws Exception 
+     * 
+     * @throws Exception
      */
     @Test
     public void testDataClassificationMetadata() throws Exception {
-        //Run this test only with newer SQL Servers (version>=2018) that support Data Classification
-        try (Connection con = DriverManager.getConnection(connectionString);
-            Statement stmt = connection.createStatement();){
-            if(serverSupportsDataClassification(stmt)) {
+        // Run this test only with newer SQL Servers (version>=2018) that support Data Classification
+        try (Connection con = DriverManager.getConnection(connectionString); Statement stmt = connection.createStatement();) {
+            if (serverSupportsDataClassification(stmt)) {
                 createTable(connection, stmt);
                 runTestsForServer(stmt);
-                dropTable(stmt);
+                Utils.dropTableIfExists(tableName, stmt);
             }
         }
     }
-    
+
     /**
      * Checks if object SYS.SENSITIVITY_CLASSIFICATIONS exists in SQL Server
+     * 
      * @param Statement
      * @return boolean
      */
     private boolean serverSupportsDataClassification(Statement stmt) {
         try {
             stmt.execute("SELECT * FROM SYS.SENSITIVITY_CLASSIFICATIONS");
-        }catch(SQLException e) {
-            if(e.getErrorCode() == 208) { 
+        }
+        catch (SQLException e) {
+            if (e.getErrorCode() == 208) {
                 return false;
             }
         }
         return true;
     }
-    
+
     /**
-     * Creates a new table in database with data classification column tags
-     * Inserts rows of data in the table
+     * Creates a new table in database with data classification column tags Inserts rows of data in the table
+     * 
      * @param connection
      * @param stmt
      * @param tableName
@@ -61,26 +64,25 @@ public class DataClassificationTest extends AbstractTest {
      */
     private void createTable(Connection connection,
             Statement stmt) throws SQLException {
-        String createQuery = "CREATE TABLE " + tableName + " (" + 
-                "[Id] [int] IDENTITY(1,1) NOT NULL," +
-                "[CompanyName] [nvarchar](40) NOT NULL," + 
-                "[ContactName] [nvarchar](50) NULL," +
-                "[ContactTitle] [nvarchar](40) NULL," +
-                "[City] [nvarchar](40) NULL," +
-                "[Country] [nvarchar](40) NULL," +
-                "[Phone] [nvarchar](30) MASKED WITH (FUNCTION = 'default()') NULL," +
-                "[Fax] [nvarchar](30) MASKED WITH (FUNCTION = 'default()') NULL)";
+        String createQuery = "CREATE TABLE " + tableName + " (" + "[Id] [int] IDENTITY(1,1) NOT NULL," + "[CompanyName] [nvarchar](40) NOT NULL,"
+                + "[ContactName] [nvarchar](50) NULL," + "[ContactTitle] [nvarchar](40) NULL," + "[City] [nvarchar](40) NULL,"
+                + "[Country] [nvarchar](40) NULL," + "[Phone] [nvarchar](30) MASKED WITH (FUNCTION = 'default()') NULL,"
+                + "[Fax] [nvarchar](30) MASKED WITH (FUNCTION = 'default()') NULL)";
         stmt.execute(createQuery);
-        
-        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName + ".CompanyName WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Company name', INFORMATION_TYPE_ID='COMPANY')");
-        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName + ".ContactName WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Person name', INFORMATION_TYPE_ID='NAME')");
-        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName + ".Phone WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Contact Information', INFORMATION_TYPE_ID='CONTACT')");
-        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName + ".Fax WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Contact Information', INFORMATION_TYPE_ID='CONTACT')");
-        
-        //INSERT ROWS OF DATA
-        try(PreparedStatement ps = connection.prepareStatement("INSERT INTO " + tableName + " VALUES (?,?,?,?,?,?,?)")) {
-            
-            ps.setString(1,"Exotic Liquids");
+
+        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName
+                + ".CompanyName WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Company name', INFORMATION_TYPE_ID='COMPANY')");
+        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName
+                + ".ContactName WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Person name', INFORMATION_TYPE_ID='NAME')");
+        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName
+                + ".Phone WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Contact Information', INFORMATION_TYPE_ID='CONTACT')");
+        stmt.execute("ADD SENSITIVITY CLASSIFICATION TO " + tableName
+                + ".Fax WITH (LABEL='PII', LABEL_ID='L1', INFORMATION_TYPE='Contact Information', INFORMATION_TYPE_ID='CONTACT')");
+
+        // INSERT ROWS OF DATA
+        try (PreparedStatement ps = connection.prepareStatement("INSERT INTO " + tableName + " VALUES (?,?,?,?,?,?,?)")) {
+
+            ps.setString(1, "Exotic Liquids");
             ps.setString(2, "Charlotte Cooper");
             ps.setObject(3, null);
             ps.setObject(4, "London");
@@ -88,8 +90,8 @@ public class DataClassificationTest extends AbstractTest {
             ps.setString(6, "(171) 555-2222");
             ps.setString(7, null);
             ps.execute();
-    
-            ps.setString(1,"New Orleans");
+
+            ps.setString(1, "New Orleans");
             ps.setString(2, "Cajun Delights");
             ps.setObject(3, null);
             ps.setObject(4, "New Orleans");
@@ -97,8 +99,8 @@ public class DataClassificationTest extends AbstractTest {
             ps.setString(6, "(100) 555-4822");
             ps.setString(7, null);
             ps.execute();
-    
-            ps.setString(1,"Grandma Kelly's Homestead");
+
+            ps.setString(1, "Grandma Kelly's Homestead");
             ps.setString(2, "Regina Murphy");
             ps.setObject(3, null);
             ps.setObject(4, "Ann Arbor");
@@ -111,6 +113,7 @@ public class DataClassificationTest extends AbstractTest {
 
     /**
      * Selects data from the table and triggers verifySensitivityClassification method
+     * 
      * @param stmt
      * @param queries
      * @throws Exception
@@ -123,36 +126,26 @@ public class DataClassificationTest extends AbstractTest {
 
     /**
      * Verifies resultset recieved to contain data classification information as set.
+     * 
      * @param rs
      * @throws SQLException
      */
-    private void verifySensitivityClassification(SQLServerResultSet rs) throws SQLException
-    {
-        if(null!= rs.getSensitivityClassification()){
-            for (int columnPos = 0; columnPos < rs.getSensitivityClassification().getColumnSensitivities().size(); columnPos++)
-            {
-                for (SensitivityProperty sp : rs.getSensitivityClassification().getColumnSensitivities().get(columnPos).getSensitivityProperties())
-                {
-                    if(columnPos==1 || columnPos==2 || columnPos == 6 || columnPos ==7) {
-                        assert(sp.getLabel() != null);
-                        assert(sp.getLabel().getId().equalsIgnoreCase("L1"));
-                        assert(sp.getLabel().getName().equalsIgnoreCase("PII"));
+    private void verifySensitivityClassification(SQLServerResultSet rs) throws SQLException {
+        if (null != rs.getSensitivityClassification()) {
+            for (int columnPos = 0; columnPos < rs.getSensitivityClassification().getColumnSensitivities().size(); columnPos++) {
+                for (SensitivityProperty sp : rs.getSensitivityClassification().getColumnSensitivities().get(columnPos).getSensitivityProperties()) {
+                    if (columnPos == 1 || columnPos == 2 || columnPos == 6 || columnPos == 7) {
+                        assert (sp.getLabel() != null);
+                        assert (sp.getLabel().getId().equalsIgnoreCase("L1"));
+                        assert (sp.getLabel().getName().equalsIgnoreCase("PII"));
 
-                        assert(sp.getInformationType() != null);
-                        assert(sp.getInformationType().getId().equalsIgnoreCase(columnPos==1?"COMPANY":(columnPos==2?"NAME":"CONTACT")));
-                        assert(sp.getInformationType().getName().equalsIgnoreCase(columnPos==1?"Company name":(columnPos==2?"Person Name":"Contact Information")));
+                        assert (sp.getInformationType() != null);
+                        assert (sp.getInformationType().getId().equalsIgnoreCase(columnPos == 1 ? "COMPANY" : (columnPos == 2 ? "NAME" : "CONTACT")));
+                        assert (sp.getInformationType().getName()
+                                .equalsIgnoreCase(columnPos == 1 ? "Company name" : (columnPos == 2 ? "Person Name" : "Contact Information")));
                     }
                 }
             }
         }
-    }
-    
-    /**
-     * Drops table from the database
-     * @param stmt
-     * @throws SQLException
-     */
-    private void dropTable(Statement stmt) throws SQLException {
-        stmt.execute("DROP TABLE " + tableName);
     }
 }
