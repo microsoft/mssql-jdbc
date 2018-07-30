3f671268bf8cfdb023dbc2a7d499ce4e76b12000
Peter Bae <v-hyba@microsoft.com>
2018-07-30 16:41:38 -0700

    Update samples from Peter

--- src/samples/datatypes/src/main/java/BasicDataTypes.java
+++ src/samples/datatypes/src/main/java/BasicDataTypes.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,6 +15,7 @@ import java.sql.Statement;
 import java.sql.Time;
 import java.sql.Timestamp;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerResultSet;
 
 import microsoft.sql.DateTimeOffset;
@@ -47,12 +47,16 @@ public class BasicDataTypes {
             System.out.print("Enter password: ");
             password = br.readLine();
 
-            // Create a variable for the connection string.
-            String connectionUrl = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";" + "databaseName="
-                    + databaseName + ";username=" + username + ";password=" + password + ";";
+            // Establish the connection.
+            SQLServerDataSource ds = new SQLServerDataSource();
+            ds.setServerName(serverName);
+            ds.setPortNumber(Integer.parseInt(portNumber));
+            ds.setDatabaseName(databaseName);
+            ds.setUser(username);
+            ds.setPassword(password);
 
             // Establish the connection.
-            try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement()) {
+            try (Connection con = ds.getConnection(); Statement stmt = con.createStatement();) {
                 dropAndCreateTable(stmt);
                 insertOriginalData(con);
 
@@ -135,7 +139,7 @@ public class BasicDataTypes {
             pstmt.setObject(6, 56.78);
             pstmt.setObject(7, new Date(1453500034839L));
             pstmt.setObject(8, new Date(1453500034839L));
-            pstmt.setObject(9, new Date(1453500034839L));
+            pstmt.setObject(9, new java.util.Date(1453500034839L));
             pstmt.setObject(10, new Date(1453500034839L));
             pstmt.setObject(11, new Date(1453500034839L));
             pstmt.execute();
