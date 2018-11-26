3a0703d1404f422e76d78fa89e9d03b0a2e402af
lilgreenbird <v-susanh@microsoft.com>
2018-11-26 12:17:05 -0800

    removed dummy file

--- src/test/java/com/microsoft/sqlserver/jdbc/unit/serial/DTOSerialTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/unit/serial/DTOSerialTest.java
@@ -65,10 +65,10 @@ public class DTOSerialTest extends AbstractTest {
                 fail("Errors are different.");
             }
             if (sqlState != ex.getSQLState()) {
-                fail("Errors are different.");
+                fail("Sql states are different.");
             }
             if (errCode != ex.getErrorCode()) {
-                fail("Errors are different.");
+                fail("Error codes are different.");
             }
         }
     }
@@ -101,31 +101,28 @@ public class DTOSerialTest extends AbstractTest {
         }
     }
 
-    /*
-     * I have created two files with wrong nano value (-1) and wrong offset (15*60) by "editing" couple of generated
-     * serialized files I have checked them in here so they can be used verify the error handling. Note the code to
-     * generate this is checked in as a text file in the same dir. DateTimeOffset.java.txt
-     */
     // Negative test cases.
     private static void verifyMessedSerialization() throws Exception {
-        // these values are from the wrongnanos.dat wrongoffset.dat files checked in with the code
-        // the values represent the serialized DTO class with invalid values.
-        byte wrongnanos[] = {-84, -19, 0, 5, 115, 114, 0, 47, 109, 105, 99, 114, 111, 115, 111, 102, 116, 46, 115, 113,
+        // serialized DTO class with wrong nano values (-1)
+        byte wrongNanos[] = {-84, -19, 0, 5, 115, 114, 0, 47, 109, 105, 99, 114, 111, 115, 111, 102, 116, 46, 115, 113,
                 108, 46, 68, 97, 116, 101, 84, 105, 109, 101, 79, 102, 102, 115, 101, 116, 36, 83, 101, 114, 105, 97,
                 108, 105, 122, 97, 116, 105, 111, 110, 80, 114, 111, 120, 121, 9, 57, 90, 0, -49, -42, -72, 50, 2, 0, 3,
                 73, 0, 13, 109, 105, 110, 117, 116, 101, 115, 79, 102, 102, 115, 101, 116, 73, 0, 5, 110, 97, 110, 111,
                 115, 74, 0, 9, 117, 116, 99, 77, 105, 108, 108, 105, 115, 120, 112, 0, 0, 3, 12, -1, -1, -1, -1, 0, 0,
                 0, 0, 0, 0, 0, 1};
-        byte wrongoffset[] = {-84, -19, 0, 5, 115, 114, 0, 47, 109, 105, 99, 114, 111, 115, 111, 102, 116, 46, 115, 113,
+
+        // serialized DTO class with wrong offset (15*60)
+        byte wrongOffset[] = {-84, -19, 0, 5, 115, 114, 0, 47, 109, 105, 99, 114, 111, 115, 111, 102, 116, 46, 115, 113,
                 108, 46, 68, 97, 116, 101, 84, 105, 109, 101, 79, 102, 102, 115, 101, 116, 36, 83, 101, 114, 105, 97,
                 108, 105, 122, 97, 116, 105, 111, 110, 80, 114, 111, 120, 121, 9, 57, 90, 0, -49, -42, -72, 50, 2, 0, 3,
                 73, 0, 13, 109, 105, 110, 117, 116, 101, 115, 79, 102, 102, 115, 101, 116, 73, 0, 5, 110, 97, 110, 111,
                 115, 74, 0, 9, 117, 116, 99, 77, 105, 108, 108, 105, 115, 120, 112, 0, 0, 3, -124, 0, 0, 1, 44, 0, 0, 0,
                 0, 0, 0, 0, 1};
-        // These two serialized forms throw the exception illegalargument
+
+        // These two serialized forms throw the exception IllegalArgumentException
         boolean exThrown = false;
         try {
-            verifyMessedSerializationHelper(wrongnanos);
+            verifyMessedSerializationHelper(wrongNanos);
         } catch (IllegalArgumentException e) {
             exThrown = true;
         }
@@ -136,7 +133,7 @@ public class DTOSerialTest extends AbstractTest {
 
         exThrown = false;
         try {
-            verifyMessedSerializationHelper(wrongoffset);
+            verifyMessedSerializationHelper(wrongOffset);
         } catch (IllegalArgumentException e) {
             exThrown = true;
         }
