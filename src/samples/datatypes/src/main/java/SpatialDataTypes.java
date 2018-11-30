e971c0e1f76a2b32ff81e2cef51ebd25f91d97d2
ulvii <v-ulibra@microsoft.com>
2018-11-30 15:40:06 -0800

    Revert "removed obsolete comment (#866)"
    This reverts commit ac77a737a287a02cf08f25a9024906d8319bd2a5.

--- src/samples/datatypes/src/main/java/SpatialDataTypes.java
+++ src/samples/datatypes/src/main/java/SpatialDataTypes.java
@@ -58,6 +58,7 @@ public class SpatialDataTypes {
             try (Connection con = ds.getConnection(); Statement stmt = con.createStatement();) {
                 dropAndCreateTable(stmt);
                 
+                // TODO: Implement Sample code
                 String geoWKT = "POINT(3 40 5 6)";
                 Geometry geomWKT = Geometry.STGeomFromText(geoWKT, 0);
                 Geography geogWKT = Geography.STGeomFromText(geoWKT, 4326);
