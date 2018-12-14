434b185787413cef7d25bba4fc180369a1f7834b
Peter Bae <v-hyba@microsoft.com>
2018-12-14 14:52:48 -0800

    remove more unnecssary imports

--- src/test/java/com/microsoft/sqlserver/testframework/AbstractTest.java
+++ src/test/java/com/microsoft/sqlserver/testframework/AbstractTest.java
@@ -6,7 +6,6 @@
 package com.microsoft.sqlserver.testframework;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
