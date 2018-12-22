33bbc8487e867782125c8fb092b7d692889838c3
Iwao AVE! <harawata@gmail.com>
2018-12-22 16:08:54 +0900

    Add support for OffsetDateTime to be passed as 'type' in ResultSet.getObject() (#830)
    * Add support for OffsetDateTime to be passed as 'type' in ResultSet.getObject()
    
    * Typo fix.
    
    * Added OffsetTime support.
    
    Allow retrieving OffsetTime out of DATETIMEOFFSET column.
    
    * Added assertions for more coverage.

--- src/test/java/com/microsoft/sqlserver/jdbc/resultset/ResultSetTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/resultset/ResultSetTest.java
@@ -24,6 +24,8 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 import java.util.TimeZone;
 import java.util.UUID;
 
@@ -289,6 +291,40 @@ public class ResultSetTest extends AbstractTest {
             } finally {
                 TestUtils.dropTableIfExists(AbstractSQLGenerator.escapeIdentifier(tableName), stmt);
                 TimeZone.setDefault(prevTimeZone);
+            }
+        }
+    }
+
+    /**
+     * Tests getObject(n, java.time.OffsetDateTime.class) and getObject(n, java.time.OffsetTime.class).
+     * 
+     * @throws SQLException
+     */
+    @Test
+    public void testGetObjectAsOffsetDateTime() throws SQLException {
+        try (Connection con = DriverManager.getConnection(connectionString); Statement stmt = con.createStatement()) {
+            final String testValue = "2018-01-02T11:22:33.123456700+12:34";
+
+            stmt.executeUpdate("CREATE TABLE " + AbstractSQLGenerator.escapeIdentifier(tableName)
+                    + " (id INT PRIMARY KEY, dto DATETIMEOFFSET, dto2 DATETIMEOFFSET)");
+            stmt.executeUpdate("INSERT INTO " + AbstractSQLGenerator.escapeIdentifier(tableName)
+                    + " (id, dto, dto2) VALUES (1, '" + testValue + "', null)");
+
+            try (ResultSet rs = stmt.executeQuery(
+                    "SELECT dto, dto2 FROM " + AbstractSQLGenerator.escapeIdentifier(tableName) + " WHERE id=1")) {
+                rs.next();
+
+                OffsetDateTime expected = OffsetDateTime.parse(testValue);
+                OffsetDateTime actual = rs.getObject(1, OffsetDateTime.class);
+                assertEquals(expected, actual);
+                assertNull(rs.getObject(2, OffsetDateTime.class));
+
+                OffsetTime expectedTime = OffsetTime.parse(testValue.split("T")[1]);
+                OffsetTime actualTime = rs.getObject(1, OffsetTime.class);
+                assertEquals(expectedTime, actualTime);
+                assertNull(rs.getObject(2, OffsetTime.class));
+            } finally {
+                TestUtils.dropTableIfExists(AbstractSQLGenerator.escapeIdentifier(tableName), stmt);
             }
         }
     }
