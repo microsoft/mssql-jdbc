d76da4f647920a62f2a00925676bee07df0582a7
Cheena Malhotra <v-chmalh@microsoft.com>
2019-03-07 10:44:31 -0800

    Add another try block for inner execution

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
@@ -5569,25 +5569,25 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
 
         try (SQLServerStatement stmt = new SQLServerStatement(this, ResultSet.TYPE_FORWARD_ONLY,
                 ResultSet.CONCUR_READ_ONLY, SQLServerStatementColumnEncryptionSetting.UseConnectionSetting)) {
+            try {
+                // If asked, limit the time to wait for the query to complete.
+                if (0 != timeout)
+                    stmt.setQueryTimeout(timeout);
 
-            // If asked, limit the time to wait for the query to complete.
-            if (0 != timeout)
-                stmt.setQueryTimeout(timeout);
-
-            // Try to execute the query. If this succeeds, then the connection is valid.
-            // If it fails (throws an exception), then the connection is not valid.
-            // If a timeout was provided, execution throws an "query timed out" exception
-            // if the query fails to execute in that time.
-            stmt.executeQueryInternal("SELECT 1");
-            isValid = true;
-        } catch (SQLException e) {
-            // Do not propagate SQLExceptions from query execution or statement closure.
-            // The connection is considered to be invalid if the statement fails to close,
-            // even though query execution succeeded.
-            connectionlogger.fine(toString() + " Exception checking connection validity: " + e.getMessage());
-            return false;
+                // Try to execute the query. If this succeeds, then the connection is valid.
+                // If it fails (throws an exception), then the connection is not valid.
+                // If a timeout was provided, execution throws an "query timed out" exception
+                // if the query fails to execute in that time.
+                stmt.executeQueryInternal("SELECT 1");
+                isValid = true;
+            } catch (SQLException e) {
+                // Do not propagate SQLExceptions from query execution or statement closure.
+                // The connection is considered to be invalid if the statement fails to close,
+                // even though query execution succeeded.
+                connectionlogger.fine(toString() + " Exception checking connection validity: " + e.getMessage());
+                return false;
+            }
         }
-
         loggerExternal.exiting(getClassNameLogging(), "isValid", isValid);
         return isValid;
     }
