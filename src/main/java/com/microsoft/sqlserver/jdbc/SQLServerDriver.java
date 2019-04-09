0ee446d2634a399a6e6b611a9617cd5f7b7da519
Peter Bae <v-hyba@microsoft.com>
2019-04-09 15:41:37 -0700

    Fix ActivityID map getting not cleaned up properly (#1028)
    Fix | Fix ActivityID map getting not cleaned up properly - hotfix 7.2.2

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerDriver.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerDriver.java
@@ -561,7 +561,7 @@ public final class SQLServerDriver implements java.sql.Driver {
         try {
             register();
         } catch (SQLException e) {
-            if (drLogger.isLoggable(Level.FINER) && Util.IsActivityTraceOn()) {
+            if (drLogger.isLoggable(Level.FINER) && Util.isActivityTraceOn()) {
                 drLogger.finer("Error registering driver: " + e);
             }
         }
