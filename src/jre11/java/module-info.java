71e538932fc69a815f7a7724bd02d80374fd0e4e
GedMarc <ged_marc@hotmail.com>
2019-04-24 01:26:57 +0200

    Strictly name the MicroSoft SQL Driver for JLink and Distribution

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,21 @@
+module com.microsoft.sqlserver.jdbc {
+	exports com.microsoft.sqlserver.jdbc;
+	exports microsoft.sql;
+
+	requires java.sql;
+	requires java.naming;
+	requires java.security.jgss;
+
+	requires static azure.client.runtime;
+	requires static azure.keyvault;
+	requires static azure.keyvault.webkey;
+	requires static retrofit2;
+	requires static okhttp3;
+	requires static client.runtime;
+	requires static adal4j;
+	requires static org.osgi.core;
+	requires static org.osgi.compendium;
+	requires activation;
+
+	provides java.sql.Driver with com.microsoft.sqlserver.jdbc.SQLServerDriver;
+}
