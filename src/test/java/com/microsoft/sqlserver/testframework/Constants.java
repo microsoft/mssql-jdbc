5e12d684124396881dae65001fc486443de5b0b0
Cheena Malhotra <v-chmalh@microsoft.com>
2019-05-08 09:59:19 -0700

    Feature | Introduce Java 12 Support to driver and build additional "jre12" JAR (#1050)

--- src/test/java/com/microsoft/sqlserver/testframework/Constants.java
+++ src/test/java/com/microsoft/sqlserver/testframework/Constants.java
@@ -14,14 +14,18 @@ public class Constants {
      * Use below tags for tests to exclude them from test group:
      * 
      * <pre>
+     * xJDBC42 - - - - - - For tests not compatible with JDBC 42 Specifications
+     * xGradle - - - - - - For tests not compatible with Gradle Script (e.g. Manifest File)
      * xSQLv12 - - - - - - For tests not compatible with SQL Server 2008 R2 - 2014
-     * xSQLv14 - - - - - - For tests not compatible with SQL Server 2008 R2 - 2017
-     * xSQLv15 - - - - - - For tests not compatible with SQL Server 2008 R2 - 2019
+     * xSQLv14 - - - - - - For tests not compatible with SQL Server 2016 - 2017
+     * xSQLv15 - - - - - - For tests not compatible with SQL Server 2019
      * xAzureSQLDB - - - - For tests not compatible with Azure SQL Database
      * xAzureSQLDW - - - - For tests not compatible with Azure Data Warehouse
      * xAzureSQLMI - - - - For tests not compatible with Azure SQL Managed Instance
      * </pre>
      */
+    public static final String xJDBC42 = "xJDBC42";
+    public static final String xGradle = "xGradle";
     public static final String xSQLv12 = "xSQLv12";
     public static final String xSQLv14 = "xSQLv14";
     public static final String xSQLv15 = "xSQLv15";
