d75424e76e45f0e955b74801d8e91dddfb8df9f7
Tariq Abughofa <tariq@upgrade.com>
2019-09-10 11:41:36 -0400

    enableing mssql data decryption on linux machine with windows certificate

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerColumnEncryptionCertificateStoreProvider.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerColumnEncryptionCertificateStoreProvider.java
@@ -25,6 +25,10 @@ public final class SQLServerColumnEncryptionCertificateStoreProvider extends SQL
     static final String currentUserDirectory = "CurrentUser";
     static final String myCertificateStore = "My";
 
+    // FOR NON WINDOWS MACHINES
+    String keyStorePath = null;
+    char[] keyStorePwd = null;
+
     static {
         if (System.getProperty("os.name").toLowerCase(Locale.ENGLISH).startsWith("windows")) {
             isWindows = true;
@@ -47,6 +51,14 @@ public final class SQLServerColumnEncryptionCertificateStoreProvider extends SQL
 
     public String getName() {
         return this.name;
+    }
+
+    public void setKeyStorePath(String ksp) {
+        this.keyStorePath = ksp;
+    }
+
+    public void setKeyStorePwd(String ksp) {
+        this.keyStorePwd = ksp.toCharArray();
     }
 
     public byte[] encryptColumnEncryptionKey(String masterKeyPath, String encryptionAlgorithm,
@@ -74,11 +86,131 @@ public final class SQLServerColumnEncryptionCertificateStoreProvider extends SQL
         if (isWindows) {
             plainCek = decryptColumnEncryptionKeyWindows(masterKeyPath, encryptionAlgorithm,
                     encryptedColumnEncryptionKey);
-        } else {
+        } else if (null != keyStorePwd && null != keyStorePath) {
+            plainCek = decryptColumnEncryptionKeyNonWindows(masterKeyPath, encryptionAlgorithm, encryptedColumnEncryptionKey);
+        }  else {
             throw new SQLServerException(SQLServerException.getErrString("R_notSupported"), null);
         }
         windowsCertificateStoreLogger.exiting(SQLServerColumnEncryptionCertificateStoreProvider.class.getName(),
                 "decryptColumnEncryptionKey", "Finished decrypting Column Encryption Key.");
         return plainCek;
+    }
+
+    private byte[] decryptColumnEncryptionKeyNonWindows(String masterKeyPath,
+                                                        String encryptionAlgorithm,
+                                                        byte[] encryptedColumnEncryptionKey) throws SQLServerException {
+        KeyStoreProviderCommon.validateNonEmptyMasterKeyPath(masterKeyPath);
+        CertificateDetails certificateDetails = getCertificateDetailsNonWin(masterKeyPath);
+        return KeyStoreProviderCommon.decryptColumnEncryptionKey(masterKeyPath, encryptionAlgorithm,
+                encryptedColumnEncryptionKey,
+                certificateDetails);
+    }
+
+    private CertificateDetails getCertificateDetailsNonWin(String masterKeyPath) throws SQLServerException {
+        String storeLocation = null;
+
+        String[] certParts = masterKeyPath.split("/");
+
+        // Validate certificate path
+        // Certificate path should only contain 3 parts (Certificate Location, Certificate Store Name and Thumbprint)
+        if (certParts.length > 3) {
+            MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_AECertpathBad"));
+            Object[] msgArgs = {masterKeyPath};
+            throw new SQLServerException(form.format(msgArgs), null);
+        }
+
+        // Extract the store location where the cert is stored
+        if (certParts.length > 2) {
+            if (certParts[0].equalsIgnoreCase(localMachineDirectory)) {
+                storeLocation = localMachineDirectory;
+            }
+            else if (certParts[0].equalsIgnoreCase(currentUserDirectory)) {
+                storeLocation = currentUserDirectory;
+            }
+            else {
+                MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_AECertLocBad"));
+                Object[] msgArgs = {certParts[0], masterKeyPath};
+                throw new SQLServerException(form.format(msgArgs), null);
+            }
+        }
+
+        // Parse the certificate store name. Only store name "My" is supported.
+        if (certParts.length > 1) {
+            if (!certParts[certParts.length - 2].equalsIgnoreCase(myCertificateStore)) {
+                MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_AECertStoreBad"));
+                Object[] msgArgs = {certParts[certParts.length - 2], masterKeyPath};
+                throw new SQLServerException(form.format(msgArgs), null);
+            }
+        }
+
+        // Get thumpbrint
+        String thumbprint = certParts[certParts.length - 1];
+        if ((null == thumbprint) || (0 == thumbprint.length())) {
+            // An empty thumbprint specified
+            MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_AECertHashEmpty"));
+            Object[] msgArgs = {masterKeyPath};
+            throw new SQLServerException(form.format(msgArgs), null);
+        }
+
+        // Find the certificate and return
+        return getCertificateByThumbprintNonWin(thumbprint, masterKeyPath);
+    }
+
+    private CertificateDetails getCertificateByThumbprintNonWin(String thumbprint,
+                                                                String masterKeyPath) throws SQLServerException {
+        FileInputStream fis;
+
+        if (null == masterKeyPath || 0 == masterKeyPath.length()) {
+            throw new SQLServerException(null, SQLServerException.getErrString("R_InvalidMasterKeyDetails"), null, 0, false);
+        }
+
+        // Loading key store as PKCS12
+        KeyStore keyStore = null;
+        try {
+            keyStore = KeyStore.getInstance("PKCS12");
+            fis = new FileInputStream(keyStorePath);
+            keyStore.load(fis, keyStorePwd);
+        } catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
+            MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_invalidKeyStoreFile"));
+            Object[] msgArgs = {keyStorePath};
+            throw new SQLServerException(form.format(msgArgs), e);
+        }
+
+        // If we are here, we were able to load a PKCS12 file.
+        try {
+            for (Enumeration<String> enumeration = keyStore.aliases(); enumeration.hasMoreElements();) {
+
+                String alias = enumeration.nextElement();
+
+                X509Certificate publicCertificate = (X509Certificate) keyStore.getCertificate(alias);
+
+                if (thumbprint.matches(getThumbPrint(publicCertificate))) {
+                    // Found the right certificate
+                    Key keyPrivate = null;
+                    try {
+                        keyPrivate = keyStore.getKey(alias, keyStorePwd);
+                        if (null == keyPrivate) {
+                            MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_UnrecoverableKeyAE"));
+                            Object[] msgArgs = {masterKeyPath};
+                            throw new SQLServerException(this, form.format(msgArgs), null, 0, false);
+                        }
+                    }
+                    catch (UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException e) {
+                        MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_UnrecoverableKeyAE"));
+                        Object[] msgArgs = {masterKeyPath};
+                        throw new SQLServerException(this, form.format(msgArgs), null, 0, false);
+                    }
+                    return new CertificateDetails(publicCertificate, keyPrivate);
+                }
+            }// end of for for alias
+        }
+        catch (CertificateException | NoSuchAlgorithmException | KeyStoreException e) {
+            MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_CertificateError"));
+            Object[] msgArgs = {masterKeyPath, name};
+            throw new SQLServerException(form.format(msgArgs), e);
+        }
+
+        // Looped over all files, haven't found the certificate
+        throw new SQLServerException(SQLServerException.getErrString("R_KeyStoreNotFound"), null);
     }
 }
