4cd2fdbacf447b88c7079a3fe5faacefc8847712
Peter Bae <v-hyba@microsoft.com>
2019-09-25 16:40:35 -0700

    add header

--- src/test/java/com/microsoft/sqlserver/jdbc/connection/XADataSourceTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/connection/XADataSourceTest.java
@@ -1,3 +1,8 @@
+/*
+ * Microsoft JDBC Driver for SQL Server Copyright(c) Microsoft Corporation All rights reserved. This program is made
+ * available under the terms of the MIT License. See the LICENSE file in the project root for more information.
+ */
+
 package com.microsoft.sqlserver.jdbc.connection;
 
 import java.io.File;
