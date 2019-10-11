8123e36020fa0a5c1e9727b51a5574f4b7bbde1e
ulvii <v-ulibra@microsoft.com>
2019-10-11 16:57:50 -0700

    Update src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
@@ -610,7 +610,7 @@ public final class SQLServerResource extends ListResourceBundle {
             {"R_EnclavePackageLengthError",
                     "More bytes from the server was recieved than expected when parsing Enclave Package."},
             {"R_EnclavePKLengthError",
-                    "More bytes from the server was recieved than expected when parsing Enclave PK."},
+                    "More bytes from the server were received than expected when parsing Enclave PK."},
             {"R_MalformedECDHPublicKey", "The ECDH public key from the server must be 104 bits in length."},
             {"R_MalformedECDHHeader", "Unexpected values for ECDH public key header from the server."},
             {"R_InvalidHealthCert",
