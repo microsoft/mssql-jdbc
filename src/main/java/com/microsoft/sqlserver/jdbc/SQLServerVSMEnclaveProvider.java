f5f582d422a4a430fa9340799913225f4ec82feb
v-reye <v-reye@microsoft.com>
2020-01-23 11:20:10 -0800

    Release | Fixed issued reported by SonarQube
    * SonarQube fixes
    
    * Fix resource leak in AE
    
    * Addressing comments
    
    * throw exception if 3rd resultset is empty

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerVSMEnclaveProvider.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerVSMEnclaveProvider.java
@@ -95,16 +95,17 @@ public class SQLServerVSMEnclaveProvider implements ISQLServerEnclaveProvider {
         return enclaveSession;
     }
 
-    private VSMAttestationResponse validateAttestationResponse(VSMAttestationResponse ar) throws SQLServerException {
-        try {
-            byte[] attestationCerts = getAttestationCertificates();
-            ar.validateCert(attestationCerts);
-            ar.validateStatementSignature();
-            ar.validateDHPublicKey();
-        } catch (IOException | GeneralSecurityException e) {
-            SQLServerException.makeFromDriverError(null, this, e.getLocalizedMessage(), "0", false);
+    private void validateAttestationResponse() throws SQLServerException {
+        if (null != hgsResponse) {
+            try {
+                byte[] attestationCerts = getAttestationCertificates();
+                hgsResponse.validateCert(attestationCerts);
+                hgsResponse.validateStatementSignature();
+                hgsResponse.validateDHPublicKey();
+            } catch (IOException | GeneralSecurityException e) {
+                SQLServerException.makeFromDriverError(null, this, e.getLocalizedMessage(), "0", false);
+            }
         }
-        return ar;
     }
 
     private static Hashtable<String, X509CertificateEntry> certificateCache = new Hashtable<>();
@@ -139,33 +140,30 @@ public class SQLServerVSMEnclaveProvider implements ISQLServerEnclaveProvider {
             String preparedTypeDefinitions, Parameter[] params,
             ArrayList<String> parameterNames) throws SQLServerException {
         ArrayList<byte[]> enclaveRequestedCEKs = new ArrayList<>();
-        ResultSet rs = null;
         try (PreparedStatement stmt = connection.prepareStatement(connection.enclaveEstablished() ? SDPE1 : SDPE2)) {
-            if (connection.enclaveEstablished()) {
-                rs = executeSDPEv1(stmt, userSql, preparedTypeDefinitions);
-            } else {
-                rs = executeSDPEv2(stmt, userSql, preparedTypeDefinitions, vsmParams);
-            }
-            if (null == rs) {
-                // No results. Meaning no parameter.
-                // Should never happen.
-                return enclaveRequestedCEKs;
-            }
-            processSDPEv1(userSql, preparedTypeDefinitions, params, parameterNames, connection, stmt, rs,
-                    enclaveRequestedCEKs);
-            // Process the third resultset.
-            if (connection.isAEv2() && stmt.getMoreResults()) {
-                rs = (SQLServerResultSet) stmt.getResultSet();
-                while (rs.next()) {
-                    hgsResponse = new VSMAttestationResponse(rs.getBytes(1));
-                    // This validates and establishes the enclave session if valid
-                    if (!connection.enclaveEstablished()) {
-                        hgsResponse = validateAttestationResponse(hgsResponse);
+            try (ResultSet rs = connection.enclaveEstablished() ? executeSDPEv1(stmt, userSql,
+                    preparedTypeDefinitions) : executeSDPEv2(stmt, userSql, preparedTypeDefinitions, vsmParams)) {
+                if (null == rs) {
+                    // No results. Meaning no parameter.
+                    // Should never happen.
+                    return enclaveRequestedCEKs;
+                }
+                processSDPEv1(userSql, preparedTypeDefinitions, params, parameterNames, connection, stmt, rs,
+                        enclaveRequestedCEKs);
+                // Process the third resultset.
+                if (connection.isAEv2() && stmt.getMoreResults()) {
+                    try (ResultSet hgsRs = (SQLServerResultSet) stmt.getResultSet()) {
+                        if (hgsRs.next()) {
+                            hgsResponse = new VSMAttestationResponse(hgsRs.getBytes(1));
+                            // This validates and establishes the enclave session if valid
+                            validateAttestationResponse();
+                        } else {
+                            SQLServerException.makeFromDriverError(null, this,
+                                    SQLServerException.getErrString("R_UnableRetrieveParameterMetadata"), "0", false);
+                        }
                     }
                 }
             }
-            // Null check for rs is done already.
-            rs.close();
         } catch (SQLException | IOException e) {
             if (e instanceof SQLServerException) {
                 throw (SQLServerException) e;
