c9f392a0bbc4bab5eab4e78abb44035efed7b1d0
rene-ye <rene-ye@users.noreply.github.com>
2020-01-29 13:18:10 -0800

    Make temp class non public

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerBouncyCastleLoader.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerBouncyCastleLoader.java
@@ -5,7 +5,7 @@ import java.security.Security;
 /*
  * Class that is meant to statically load the BouncyCastle Provider for JDK 8. Hides the call so JDK 11/13 don't have to include the dependency.
  */
-public class SQLServerBouncyCastleLoader {
+class SQLServerBouncyCastleLoader {
     static void loadBouncyCastle() {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
     }
