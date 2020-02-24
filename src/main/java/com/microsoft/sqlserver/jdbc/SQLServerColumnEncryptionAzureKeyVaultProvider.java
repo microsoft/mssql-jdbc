f006c4eb7daf92211d90bdb5f0c088eb50969914
ulvii <v-ulibra@microsoft.com>
2020-02-24 12:22:09 -0800

    Added new Azure endpoints to the list of trusted endpoints (#1264)

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerColumnEncryptionAzureKeyVaultProvider.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerColumnEncryptionAzureKeyVaultProvider.java
@@ -56,7 +56,9 @@ public class SQLServerColumnEncryptionAzureKeyVaultProvider extends SQLServerCol
     private final String azureTrustedEndpoints[] = {"vault.azure.net", // default
             "vault.azure.cn", // Azure China
             "vault.usgovcloudapi.net", // US Government
-            "vault.microsoftazure.de" // Azure Germany
+            "vault.microsoftazure.de", // Azure Germany
+            "vault.cloudapi.eaglex.ic.gov",
+            "vault.cloudapi.microsoft.scloud",
     };
 
     private final String rsaEncryptionAlgorithmWithOAEPForAKV = "RSA-OAEP";
