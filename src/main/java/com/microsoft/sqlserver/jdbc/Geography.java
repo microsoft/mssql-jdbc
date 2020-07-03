c82222a001c0b9dd87bb74378bb9785c55cd65ac
Peter Bae <v-hyba@microsoft.com>
2020-07-03 16:39:32 -0700

    Fix STAsBinary to return WKB format instead of CLR (#1364)

--- src/main/java/com/microsoft/sqlserver/jdbc/Geography.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/Geography.java
@@ -38,28 +38,28 @@ public class Geography extends SQLServerSpatialDatatype {
 
         parseWKTForSerialization(this, currentWktPos, -1, false);
 
-        serializeToWkb(false, this);
+        serializeToClr(false, this);
         isNull = false;
     }
 
     /**
-     * Private constructor used for creating a Geography object from WKB.
+     * Private constructor used for creating a Geography object from internal SQL Server format.
      * 
-     * @param wkb
-     *        Well-Known Binary (WKB) provided by the user.
+     * @param clr
+     *        Internal SQL Server format provided by the user.
      * @throws SQLServerException
      *         if an exception occurs
      */
-    protected Geography(byte[] wkb) throws SQLServerException {
-        if (null == wkb || wkb.length <= 0) {
-            throwIllegalWKB();
+    protected Geography(byte[] clr) throws SQLServerException {
+        if (null == clr || clr.length <= 0) {
+            throwIllegalByteArray();
         }
 
-        this.wkb = wkb;
-        buffer = ByteBuffer.wrap(wkb);
+        this.clr = clr;
+        buffer = ByteBuffer.wrap(clr);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
 
-        parseWkb(this);
+        parseClr(this);
 
         WKTsb = new StringBuffer();
         WKTsbNoZM = new StringBuffer();
@@ -91,6 +91,11 @@ public class Geography extends SQLServerSpatialDatatype {
      * Constructor for a Geography instance from an Open Geospatial Consortium (OGC) Well-Known Binary (WKB)
      * representation.
      * 
+     * Note: This method currently uses internal SQL Server format (CLR) to create a Geography instance,
+     * but in the future this will be changed to accept WKB data instead, as the SQL Server counterpart of this
+     * method (STGeomFromWKB) uses WKB.
+     * For existing users who are already using this method, consider switching to deserialize(byte) instead.
+     * 
      * @param wkb
      *        Well-Known Binary (WKB) provided by the user.
      * @return Geography Geography instance created from WKB
@@ -104,14 +109,14 @@ public class Geography extends SQLServerSpatialDatatype {
     /**
      * Constructor for a Geography instance from an internal SQL Server format for spatial data.
      * 
-     * @param wkb
-     *        Well-Known Binary (WKB) provided by the user.
-     * @return Geography Geography instance created from WKB
+     * @param clr
+     *        Internal SQL Server format provided by the user.
+     * @return Geography Geography instance created from clr
      * @throws SQLServerException
      *         if an exception occurs
      */
-    public static Geography deserialize(byte[] wkb) throws SQLServerException {
-        return new Geography(wkb);
+    public static Geography deserialize(byte[] clr) throws SQLServerException {
+        return new Geography(clr);
     }
 
     /**
@@ -156,10 +161,10 @@ public class Geography extends SQLServerSpatialDatatype {
      */
     public String STAsText() throws SQLServerException {
         if (null == wktNoZM) {
-            buffer = ByteBuffer.wrap(wkb);
+            buffer = ByteBuffer.wrap(clr);
             buffer.order(ByteOrder.LITTLE_ENDIAN);
 
-            parseWkb(this);
+            parseClr(this);
 
             WKTsb = new StringBuffer();
             WKTsbNoZM = new StringBuffer();
@@ -176,10 +181,10 @@ public class Geography extends SQLServerSpatialDatatype {
      * @return byte array representation of the Geography object.
      */
     public byte[] STAsBinary() {
-        if (null == wkbNoZM) {
-            serializeToWkb(true, this);
+        if (null == wkb) {
+            serializeToWkb(this);
         }
-        return wkbNoZM;
+        return wkb;
     }
 
     /**
@@ -188,7 +193,7 @@ public class Geography extends SQLServerSpatialDatatype {
      * @return byte array representation of the Geography object.
      */
     public byte[] serialize() {
-        return wkb;
+        return clr;
     }
 
     /**
