6a545a19799279b30a49b413884f0af7e9e8e7c2
ulvii <v-ulibra@microsoft.com>
2020-08-21 16:55:56 -0700

    remove scripts

--- src/test/scripts/getCEKEncryptedValue.ps1
+++
@@ -1,4 +0,0 @@
-param([Parameter()] [ValidateNotNullOrEmpty()] [string]$thumbprint=$(throw "thumbprint is mandatory, please provide a value."))
-Install-Module -Name SqlServer -AllowClobber -Force -Scope CurrentUser
-$TargetCmkSettings = New-SqlCertificateStoreColumnMasterKeySettings -CertificateStoreLocation "CurrentUser" -Thumbprint "$thumbprint"
-New-SqlColumnEncryptionKeyEncryptedValue -TargetColumnMasterKeySettings $TargetCmkSettings
