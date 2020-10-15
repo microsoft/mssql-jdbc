af76d7af3846801be86f7f94fbc3318b1ed5bc04
ulvii <v-ulibra@microsoft.com>
2020-10-14 17:55:14 -0700

    Make adal4j test dependency only (#1447)

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
@@ -434,7 +434,6 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
     class ActiveDirectoryAuthentication {
         static final String JDBC_FEDAUTH_CLIENT_ID = "7f98cb04-cd1e-40df-9140-3bf7e2cea4db";
         static final String AZURE_REST_MSI_URL = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01";
-        static final String ADAL_GET_ACCESS_TOKEN_FUNCTION_NAME = "ADALGetAccessToken";
         static final String ACCESS_TOKEN_IDENTIFIER = "\"access_token\":\"";
         static final String ACCESS_TOKEN_EXPIRES_IN_IDENTIFIER = "\"expires_in\":\"";
         static final String ACCESS_TOKEN_EXPIRES_ON_IDENTIFIER = "\"expires_on\":\"";
@@ -4441,7 +4440,7 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
         while (true) {
             if (authenticationString.equalsIgnoreCase(SqlAuthentication.ActiveDirectoryPassword.toString())) {
                 if (!msalContextExists()) {
-                    MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_MSALLMissing"));
+                    MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_MSALMissing"));
                     throw new SQLServerException(form.format(new Object[] {authenticationString}), null, 0, null);
                 }
                 fedAuthToken = SQLServerMSAL4JUtils.getSqlFedAuthToken(fedAuthInfo, user,
@@ -4505,7 +4504,7 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
                             SQLServerException middleException = new SQLServerException(form.format(msgArgs1),
                                     adalException);
 
-                            form = new MessageFormat(SQLServerException.getErrString("R_ADALExecution"));
+                            form = new MessageFormat(SQLServerException.getErrString("R_MSALExecution"));
                             Object[] msgArgs = {user, authenticationString};
                             throw new SQLServerException(form.format(msgArgs), null, 0, middleException);
                         }
@@ -4532,7 +4531,7 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
                 else {
                     // Check if MSAL4J library is available
                     if (!msalContextExists()) {
-                        MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_DLLandMSALLMissing"));
+                        MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_DLLandMSALMissing"));
                         Object[] msgArgs = {SQLServerDriver.AUTH_DLL_NAME, authenticationString};
                         throw new SQLServerException(form.format(msgArgs), null, 0, null);
                     }
