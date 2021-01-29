d841e4fd1bad2dd126363c55818d876a9807f97e
Rene Ye <rene.ye@hotmail.com>
2021-01-29 15:05:38 -0800

    Clear batchparams

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
@@ -364,6 +364,7 @@ public class SQLServerPreparedStatement extends SQLServerStatement implements IS
         loggerExternal.entering(getClassNameLogging(), "clearParameters");
         checkClosed();
         encryptionMetadataIsRetrieved = false;
+        cryptoMetaBatch.clear();
         int i;
         if (inOutParam == null)
             return;
