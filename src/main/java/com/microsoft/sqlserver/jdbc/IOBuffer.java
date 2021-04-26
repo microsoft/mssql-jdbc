ae65652f0edaaca44d2eee4a4b3f44d2233d9860
lilgreenbird <v-susanh@microsoft.com>
2021-04-26 15:41:31 -0700

    Fixes trustStorePassword set to null when using applicationIntent=ReadOnly (#1565)

--- src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
@@ -1735,10 +1735,6 @@ final class TDSChannel implements Serializable {
                     try {
                         ks.load(is, (null == trustStorePassword) ? null : trustStorePassword.toCharArray());
                     } finally {
-                        // We are done with the trustStorePassword (if set). Clear it for better security.
-                        con.activeConnectionProperties
-                                .remove(SQLServerDriverStringProperty.TRUST_STORE_PASSWORD.toString());
-
                         // We are also done with the trust store input stream.
                         if (null != is) {
                             try {
@@ -4030,8 +4026,7 @@ final class TDSWriter {
 
         do {
             // Read in next chunk
-            for (charsToWrite = 0; -1 != charsRead && charsToWrite < currentPacketSize;
-                    charsToWrite += charsRead) {
+            for (charsToWrite = 0; -1 != charsRead && charsToWrite < currentPacketSize; charsToWrite += charsRead) {
                 try {
                     charsRead = reader.read(streamCharBuffer, charsToWrite, currentPacketSize - charsToWrite);
                 } catch (IOException e) {
