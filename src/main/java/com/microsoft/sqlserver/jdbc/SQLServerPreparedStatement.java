2f787087d385ef3c5104187b854498799b14ba9b
lilgreenbird <v-susanh@microsoft.com>
2021-06-03 03:16:15 -0700

    undo change

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
@@ -373,7 +373,7 @@ public class SQLServerPreparedStatement extends SQLServerStatement implements IS
         loggerExternal.entering(getClassNameLogging(), "clearParameters");
         checkClosed();
         encryptionMetadataIsRetrieved = false;
-	cryptoMetaBatch.clear();
+        cryptoMetaBatch.clear();
         int i;
         if (inOutParam == null)
             return;
