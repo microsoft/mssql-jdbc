e8e261deea0e083d48a6b34bfddad0a80c3efcd2
Karina Zhou <v-jizho2@microsoft.com>
2021-08-17 11:55:59 -0700

    Address more comments

--- src/main/java/com/microsoft/sqlserver/jdbc/SimpleTtlCache.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SimpleTtlCache.java
@@ -12,8 +12,16 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+/**
+ * Internal class implements a cache data structure based on ConcurrentHashMap. The cache uses a scheduler to 
+ * provide eviction for the cache entry according to the pre-defined time-to-live value.
+ * 
+ * This class is used as local caches for the column encrypted keys and the results of signature verification of 
+ * column master key metadata. Refer to SQLServerColumnEncryptionAzureKeyVaultProvider for an example.
+ */
 final class SimpleTtlCache<K,V> {    
 
+    // This class clears cache entry when it is called by scheduler. 
     class CacheClear implements Runnable {
     
         private K keylookupValue;
@@ -46,7 +54,7 @@ final class SimpleTtlCache<K,V> {
     static final private java.util.logging.Logger simpleCacheLogger = java.util.logging.Logger.getLogger("com.microsoft.sqlserver.jdbc.SimpleTtlCache");
 
     private final ConcurrentHashMap<K, V> cache;
-    private Duration cacheTtl = Duration.ofHours(2);    
+    private Duration cacheTtl = Duration.ofHours(2);  // The default time-to-live is set to 2 hours.
 
     private static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1, new ThreadFactory() {
         @Override
@@ -57,46 +65,102 @@ final class SimpleTtlCache<K,V> {
         }
     });
 
+    /**
+     * Get the cache size.
+     * 
+     * @return cache size
+     */
     int getCacheSize() {
         return cache.size();
     }
 
+    /**
+     * Set time-to-live value.
+     * 
+     * @param duration
+     *        time-to-live value in duration
+     */
     void setCacheTtl(Duration duration) {
         cacheTtl = duration;
     }
 
+    /**
+     * Set time-to-live value.
+     * 
+     * @param seconds
+     *        time-to-live value in seconds
+     */
     void setCacheTtl(long seconds) {
         cacheTtl = Duration.ofSeconds(seconds);
     }
 
+    /**
+     * Get the time-to-live value.
+     * 
+     * @return time-to-live in duration
+     */
     Duration getCacheTtl() {
         return cacheTtl;
     }
 
+    /**
+     * Constructor used for creating a cache instance.
+     * 
+     * Use default time-to-live value.
+     */
     SimpleTtlCache() {
         cache = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Constructor used for creating a cache instance.
+     * 
+     * @param duration
+     *        time-to-live in duration
+     */
     SimpleTtlCache(Duration duration) {
         cacheTtl = duration;
         cache = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Check if key exists.
+     * 
+     * @param key
+     *        key
+     * @return true if existed or false if not.
+     */
     boolean contains(Object key) {
         return cache.containsKey(key);
     }
 
+    /**
+     * Get value from cache based on the key given.
+     * 
+     * @param key
+     *        key
+     * @return value
+     */
     V get(Object key) {
         return cache.get(key);
     }
 
+    /**
+     * Put (Key, Value) entry into cache.
+     * 
+     * @param key
+     *        key
+     * @param value
+     *        value
+     * @return value
+     */
     V put(K key, V value) {
         V previousValue = null;
         long cacheTtlInSeconds = cacheTtl.getSeconds();
         
         if (0 < cacheTtlInSeconds) {
             previousValue = cache.put(key, value);
-            if (simpleCacheLogger.isLoggable(java.util.logging.Level.FINE)) {
+            if (simpleCacheLogger.isLoggable(java.util.logging.Level.FINEST)) {
                 simpleCacheLogger.fine("Adding encryption key to cache...");
             }
             scheduler.schedule(new CacheClear(key), cacheTtlInSeconds, SECONDS);
