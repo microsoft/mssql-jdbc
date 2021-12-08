bb3fd7620f93237fd8d6fa495776f92bc8e4f6a9
Wei Zhang <v-zhangw@microsoft.com>
2021-12-08 09:52:22 -0800

    Added logging

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
@@ -3219,6 +3219,9 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
         } catch (InternalError e) {
             // Java's NativeSeedGenerator can sometimes fail on getSeedBytes(). Exact reason is unknown but high system
             // load seems to contribute to likelihood. Retry once to mitigate.
+            if (connectionlogger.isLoggable(Level.WARNING)) {
+                connectionlogger.warning(toString() + " Generating a random UUID has failed due to : " + e.getMessage() + "Retrying once.");
+            }
             clientConnectionId = UUID.randomUUID();
         }
 
