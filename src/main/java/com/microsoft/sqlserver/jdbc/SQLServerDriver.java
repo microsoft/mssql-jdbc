d9a07bd748317bf82b9b0a734ddc623407400268
Terry <32403408+tkyc@users.noreply.github.com>
2022-03-08 16:36:00 -0800

    10.3 test fixes (#1762)
    * Eliminated case sensitivity check for prepare method
    
    * Added semi-colon delimiter for test fix

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerDriver.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerDriver.java
@@ -353,7 +353,7 @@ enum PrepareMethod {
         assert value != null;
 
         for (PrepareMethod prepareMethod : PrepareMethod.values()) {
-            if (prepareMethod.toString().equals(value)) {
+            if (prepareMethod.toString().equalsIgnoreCase(value)) {
                 return prepareMethod;
             }
         }
