465267cf7625101c160f08d52291635b47b1b64b
lilgreenbird <v-susanh@microsoft.com>
2022-07-07 00:57:46 -0700

    update

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResultSet.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResultSet.java
@@ -381,10 +381,14 @@ public class SQLServerResultSet implements ISQLServerResultSet, java.io.Serializ
                 // following the column metadata indicates an empty result set.
                 rowCount = 0;
 
+                int packetType = tdsReader.peekTokenType();
                 short status = tdsReader.peekStatusFlag();
-                StreamDone doneToken = new StreamDone();
-                doneToken.setFromTDS(tdsReader);
-                stmt.connection.getSessionRecovery().decrementUnprocessedResponseCount();
+
+                if (TDS.TDS_DONE == packetType) {
+                    StreamDone doneToken = new StreamDone();
+                    doneToken.setFromTDS(tdsReader);
+                    stmt.connection.getSessionRecovery().decrementUnprocessedResponseCount();
+                }
 
                 if ((status & TDS.DONE_ERROR) != 0 || (status & TDS.DONE_SRVERROR) != 0) {
                     MessageFormat form = new MessageFormat(SQLServerException.getErrString("R_serverError"));
