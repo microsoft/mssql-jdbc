a6c2a89562de24002419dcef37af936ebd5b1e10
lilgreenbird <v-susanh@microsoft.com>
2022-07-20 14:33:03 -0700

    removed assert (#1872)

--- src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/IOBuffer.java
@@ -7916,7 +7916,6 @@ abstract class TDSCommand implements Serializable {
      * After this call, the attention ack should no longer be expected.
      */
     final void onAttentionAck() {
-        assert attentionPending;
         attentionPending = false;
     }
 
