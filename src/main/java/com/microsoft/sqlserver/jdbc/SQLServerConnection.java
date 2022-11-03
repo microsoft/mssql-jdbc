1572bf66580b9fe085f9834c0dc23ec510fa88aa
Terry <32403408+tkyc@users.noreply.github.com>
2022-11-03 16:23:47 -0700

    Fixed mixing date import (#1954)

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
@@ -47,6 +47,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.Date;
 import java.util.logging.Level;
 
 import javax.sql.XAConnection;
