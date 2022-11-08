4e31b3c1f5f7524097aaeb9845acce1864ffcd9d
Terry <32403408+tkyc@users.noreply.github.com>
2022-11-08 09:05:33 -0800

    Version bump (#1957)

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLJdbcVersion.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLJdbcVersion.java
@@ -7,7 +7,7 @@ package com.microsoft.sqlserver.jdbc;
 
 final class SQLJdbcVersion {
     static final int major = 12;
-    static final int minor = 1;
+    static final int minor = 2;
     static final int patch = 0;
     static final int build = 0;
     /*
