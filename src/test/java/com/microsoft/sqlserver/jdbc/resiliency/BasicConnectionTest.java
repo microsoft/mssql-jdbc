86b2b882c92916d9c811b8f3a608781014af965d
lilgreenbird <v-susanh@microsoft.com>
2022-12-02 18:13:33 -0800

    cleanup unused

--- src/test/java/com/microsoft/sqlserver/jdbc/resiliency/BasicConnectionTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/resiliency/BasicConnectionTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
