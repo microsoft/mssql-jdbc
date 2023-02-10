0e782e0358f03c643a94ebf68ee55fad6f19fbaa
Terry Chow <terrykyc.7000@gmail.com>
2023-02-09 16:19:08 -0800

    Fixed imports

--- src/test/java/com/microsoft/sqlserver/jdbc/fedauth/PooledConnectionTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/fedauth/PooledConnectionTest.java
@@ -4,7 +4,9 @@
  */
 package com.microsoft.sqlserver.jdbc.fedauth;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.reflect.Field;
