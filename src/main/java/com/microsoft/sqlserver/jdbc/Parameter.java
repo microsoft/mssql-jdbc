f381a1ac6e0255499afc5ddc5d7dc396e471c279
Jeff Wasty <v-jeffwasty@microsoft.com>
2023-05-04 09:32:37 -0700

    Removed unused import

--- src/main/java/com/microsoft/sqlserver/jdbc/Parameter.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/Parameter.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.ResultSet;
