01491d5f3e8cded8241733b6debf486d5b325595
Terry Chow <v-terrychow@microsoft.com>
2023-09-08 12:34:43 -0700

    Updated test to test erroring out on retrieving out param value

--- src/test/java/com/microsoft/sqlserver/jdbc/unit/statement/StatementTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/unit/statement/StatementTest.java
@@ -1432,7 +1432,7 @@ public class StatementTest extends AbstractTest {
          */
         @Test
         @Tag(Constants.xAzureSQLDW)
-        public void testResultSetErrors() throws Exception {
+        public void testRetrievingRegisteredOutParamWhenResultSetDoesNotExists() throws Exception {
             try (Connection con = getConnection();
                     Statement stmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
 
@@ -1449,7 +1449,11 @@ public class StatementTest extends AbstractTest {
 
                     try (ResultSet rs = cstmt.executeQuery()) {} catch (Exception ex) {} ;
 
-                    assertEquals(null, cstmt.getString(2), TestResource.getResource("R_valueNotMatch"));
+                    try {
+                        cstmt.getString(2);
+                        fail(TestResource.getResource("R_expectedExceptionNotThrown"));
+                    } catch (Exception e) {
+                    }
                 }
             }
         }
