e22add71c6b6fcdbe36f639a7e5c58c6ff97297e
Jeff Wasty <v-jeffwasty@microsoft.com>
2023-10-11 14:55:11 -0700

    Adjusted timing again

--- src/test/java/com/microsoft/sqlserver/jdbc/SQLServerConnectionTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/SQLServerConnectionTest.java
@@ -478,10 +478,13 @@ public class SQLServerConnectionTest extends AbstractTest {
             long totalTime = System.currentTimeMillis() - timerStart;
             int expectedMinimumTimeInMillis = (connectRetryCount * connectRetryInterval) * 1000; // 3 seconds
 
+            System.out.println("TOTAL TIME: " + totalTime);
+
             // Minimum time is 0 seconds per attempt and connectRetryInterval * connectRetryCount seconds of interval.
             // Maximum is unknown, but is needs to be less than longLoginTimeout or else this is an issue.
             assertTrue(totalTime > expectedMinimumTimeInMillis, TestResource.getResource("R_executionNotLong"));
-            assertTrue(totalTime < (longLoginTimeout * 1000L), TestResource.getResource("R_executionTooLong"));
+            assertTrue(totalTime < 0.9 * (longLoginTimeout * 1000L), TestResource.getResource("R_executionTooLong"));
+
         }
     }
 
@@ -513,8 +516,10 @@ public class SQLServerConnectionTest extends AbstractTest {
             assertTrue(e.getMessage().contains(TestResource.getResource("R_cannotOpenDatabase")), e.getMessage());
             long totalTime = System.currentTimeMillis() - timerStart;
 
+            System.out.println("TOTAL TIME: " + totalTime);
+
             // Maximum is unknown, but is needs to be less than longLoginTimeout or else this is an issue.
-            assertTrue(totalTime < (longLoginTimeout * 1000L), TestResource.getResource("R_executionTooLong"));
+            assertTrue(totalTime < 0.9 * (longLoginTimeout * 1000L), TestResource.getResource("R_executionTooLong"));
         }
     }
 
