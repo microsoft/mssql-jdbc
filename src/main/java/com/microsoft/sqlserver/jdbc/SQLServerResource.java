729520d05c02e22c75be5f0e168f7cd4d454958c
Jeffery Wasty <v-jeffwasty@microsoft.com>
2023-11-03 13:47:04 -0700

    Check for maximum scale in IOBuffer before array copy (#2239)
    * Simple check to see if the input exceeds scale of 38.
    
    * Added test
    
    * Update failing tests
    
    * Cleanup
    
    * More cleanup
    
    * Move to TVPTypesTest
    
    * The scale check was somehow failing coco, instead moved to just catch the IndexOutOfBoundsException
    
    * ArrayIndexOutOfBoundsException not IndexOutOfBoundsException
    
    * More general
    
    * Revert
    
    * This should have been rolled back in the retry PR
    
    * This test will be removed in a separate PR
    
    * ...
    
    * Changed the error message
    
    * Forgot to change the test as well
    
    * Better error

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerResource.java
@@ -540,7 +540,8 @@ public final class SQLServerResource extends ListResourceBundle {
         {"R_serverError", "An error occurred during the current command (Done status {0}). {1}"},
         {"R_ManagedIdentityTokenAcquisitionFail", "Failed to acquire managed identity token. Request for the token succeeded, but no token was returned. The token is null."},
         {"R_AmbiguousRowUpdate", "Failed to execute updateRow(). The update is attempting an ambiguous update on tables \"{0}\" and \"{1}\". Ensure all columns being updated prior to the updateRow() call belong to the same table."},
-        {"R_InvalidSqlQuery", "Invalid SQL Query: {0}"}
+        {"R_InvalidSqlQuery", "Invalid SQL Query: {0}"},
+        {"R_InvalidScale", "Scale of input value is larger than the maximum allowed by SQL Server."}
     };
 }
 // @formatter:on
