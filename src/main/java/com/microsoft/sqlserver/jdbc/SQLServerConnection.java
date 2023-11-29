3102c11e13a1ecca505e9a08179545305fc12ca9
v-susanh@microsoft.com <v-susanh@microsoft.com>
2023-11-29 13:46:51 -0800

    test change

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerConnection.java
@@ -3283,7 +3283,7 @@ public class SQLServerConnection implements ISQLServerConnection, java.io.Serial
                                                                                                  // packetsize, etc)
                         || (SQLServerException.ERROR_SOCKET_TIMEOUT == driverErrorCode // socket timeout
                                 && (!isDBMirroring || attemptNumber > 0) // attempt at least once for mirroring
-                                && (!useTnir || attemptNumber > 0) // need to attempt at least once for TNIR
+                                && (!useTnir || attemptNumber > 0) // attempt at least once for TNIR
                                 && (connectRetryCount != 0 && attemptNumber >= connectRetryCount) // no retries left
                                 || timerHasExpired(timerExpire)) // no time left
                 ) {
