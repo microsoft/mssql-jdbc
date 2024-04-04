0dc5b6174ee29d03aa99b1897dc03e137960b820
CodegassW <wchweichenhao@gmail.com>
2024-04-04 11:23:03 -0400

    Add Assertion to the SharedTimerTest

--- src/test/java/com/microsoft/sqlserver/jdbc/SharedTimerTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/SharedTimerTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 class SharedTimerTest {
 
     @Test
     void getTimer() throws InterruptedException, ExecutionException, TimeoutException {
         final int iterations = 500;
-
         ExecutorService executor = Executors.newFixedThreadPool(2);
+
         try {
             ArrayList<CompletableFuture<?>> futures = new ArrayList<>(iterations);
             for (int i = 0; i < iterations; i++) {
@@ -22,6 +24,11 @@ class SharedTimerTest {
             CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get(2, TimeUnit.MINUTES);
         } finally {
             executor.shutdown();
+            if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
         }
+
+        assertFalse(SharedTimer.isRunning(), "SharedTimer should be stopped after all references are removed.");
     }
 }
