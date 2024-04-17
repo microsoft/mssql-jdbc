acd75d86bef46b1639341a3114ba781f5b6345bf
CodegassW <wchweichenhao@gmail.com>
2024-04-17 14:01:12 -0400

    Refactor SharedTimerTest with import and wait time, add error string to TestResource

--- src/test/java/com/microsoft/sqlserver/jdbc/SharedTimerTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/SharedTimerTest.java
@@ -1,34 +1,40 @@
 package com.microsoft.sqlserver.jdbc;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.concurrent.*;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.jupiter.api.Test;
 
 class SharedTimerTest {
 
-    @Test
-    void getTimer() throws InterruptedException, ExecutionException, TimeoutException {
-        final int iterations = 500;
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+	@Test
+	void getTimer() throws InterruptedException, ExecutionException, TimeoutException {
+		final int iterations = 500;
+		ExecutorService executor = Executors.newFixedThreadPool(2);
 
-        try {
-            ArrayList<CompletableFuture<?>> futures = new ArrayList<>(iterations);
-            for (int i = 0; i < iterations; i++) {
-                futures.add(CompletableFuture.runAsync(() -> SharedTimer.getTimer().removeRef(), executor));
-            }
+		try {
+			ArrayList<CompletableFuture<?>> futures = new ArrayList<>(iterations);
+			for (int i = 0; i < iterations; i++) {
+				futures.add(CompletableFuture.runAsync(() -> SharedTimer.getTimer().removeRef(), executor));
+			}
 
-            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get(2, TimeUnit.MINUTES);
-        } finally {
-            executor.shutdown();
-            if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                executor.shutdownNow();
-            }
-        }
+			CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get(2, TimeUnit.MINUTES);
+		} finally {
+			executor.shutdown();
+			// 5000ms wait time for the AzureDB connection to close, need full test in the
+			// test lab for the exact time
+			if (!executor.awaitTermination(5000, TimeUnit.MILLISECONDS)) {
+				executor.shutdownNow();
+			}
+		}
 
-        assertFalse(SharedTimer.isRunning(), "SharedTimer should be stopped after all references are removed.");
-    }
+		assertFalse(SharedTimer.isRunning(), TestResource.getResource("R_sharedTimerStopOnNoRef"));
+	}
 }
