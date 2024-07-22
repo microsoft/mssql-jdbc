fb69c03ff0d2d743711cb58f77270618b5399022
Terry Chow <v-terrychow@microsoft.com>
2024-07-22 14:15:18 -0700

    Corrected inefficient regex p2

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerPreparedStatement.java
@@ -139,8 +139,8 @@ public class SQLServerPreparedStatement extends SQLServerStatement implements IS
      * Regex for JDBC 'call' escape syntax
      */
     private static final Pattern callEscapePattern = Pattern
-            .compile("^\\s*(?i)\\{(\\s*\\??\\s*=?\\s*)call [^\\(\\)]+\\s*((\\(\\s*(@[^\\(\\)\\s]+\\s*" +
-                    "=\\s*)?\\?\\s*(,\\s*(@[^\\(\\)\\s]+\\s*=\\s*)?\\?\\s*)*\\))?|\\(\\))\\s*}");
+            .compile("^\\s*(?i)\\{(\\s*\\??\\s*=?\\s*)call [^\\(\\)]+\\s*" +
+                    "((\\(\\s*(@.+\\s*=\\s*)?\\?\\s*(,\\s*(@.+\\s*=\\s*)?\\?\\s*)*\\))?|\\(\\))\\s*}");
 
     /**
      * Regex for 'exec' escape syntax
