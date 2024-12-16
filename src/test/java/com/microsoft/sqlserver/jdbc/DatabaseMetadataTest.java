3df93422cf7ccd82c163b622b89df181f124e5d4
Ananya Garg <ananya20.garg@gmail.com>
2024-12-16 15:23:52 +0530

    Update DatabaseMetadataTest.java

--- src/test/java/com/microsoft/sqlserver/jdbc/DatabaseMetadataTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/DatabaseMetadataTest.java
@@ -37,7 +37,7 @@ public class DatabaseMetadataTest extends AbstractTest {
 		assertNull(connection.getWarnings(), "Expecting NO SQLWarnings from 'create table', at Connection.");
                 assertNull(stmt.getWarnings(), "Expecting NO SQLWarnings from 'create table', at Statement.");
 
-        	String createIndexSQL = "CREATE COLUMNSTORE INDEX " + indexName + " ON " + tableName;
+		String createIndexSQL = "CREATE COLUMNSTORE INDEX " + indexName + " ON " + tableName + " (" + col2Name + ")";
         	stmt.executeUpdate(createIndexSQL);
 		assertNull(connection.getWarnings(), "Expecting NO SQLWarnings from 'create index', at Connection.");
                 assertNull(stmt.getWarnings(), "Expecting NO SQLWarnings from 'create index', at Statement.");
