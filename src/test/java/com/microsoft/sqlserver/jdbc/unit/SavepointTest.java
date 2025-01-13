4b306ea13fe5dc05cb1d00ca0f1b2dad4ea9a57a
machavan <machavan@microsoft.com>
2025-01-13 17:17:21 +0530

    Fixed exception type to SQLfeatureNotSupported

--- src/test/java/com/microsoft/sqlserver/jdbc/unit/SavepointTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/unit/SavepointTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.text.MessageFormat;
 
 import com.microsoft.sqlserver.jdbc.TestUtils;
@@ -136,4 +137,21 @@ public class SavepointTest extends AbstractTest {
             } catch (SQLException e) {}
         }
     }
+
+    /**
+     * Test releaseSavepoint.
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void testReleaseSavepoint() throws SQLException {
+        try (Connection connection = getConnection()) {
+            try {
+                connection.releaseSavepoint(null);
+            } catch (SQLException e) {
+                assertTrue(e instanceof SQLFeatureNotSupportedException);
+            }
+        }
+    }
+
 }
