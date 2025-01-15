2a3d3728a61a2b9c27f11f6dc8bf59089d5ca79d
Mahendra Chavan <machavan@microsoft.com>
2025-01-15 09:54:14 +0530

    Introduced timeouts for MSAL calls. (#2562)
    * Introduced timeouts for MSAL calls.
    
    * Fixed indentation issues.
    
    * Added unit tests
    
    * Added a max wait duration of 20 seconds to MSAL calls
    
    - Added more tests
    - Improved test to check for specific error message
    
    * Added Timeout Exception catch clause for one of the auth methods
    
    * Replaced lock with tryLock.
    
    - Replaced lock with tryLock to avoid potential long waiting for other
      threads while one thread is taking long to complete.
    
    * Replaced lock with semaphore for beter readablility.
    
    - Added detailed comment for the usage of semaphore.
    
    * Renamed semAcquired to isSemAcquired
    
    * Fixed indentation for an existing code line
    
    * Change to use Mono::timeout method
    
    * Updated TOKEN_WAIT_DURATION_MS to correct value.
    
    * Improved error messages

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLServerSecurityUtility.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLServerSecurityUtility.java
@@ -8,6 +8,8 @@ package com.microsoft.sqlserver.jdbc;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Optional;
@@ -55,6 +57,8 @@ class SQLServerSecurityUtility {
     private static final HashMap<String, Credential> CREDENTIAL_CACHE = new HashMap<>();
 
     private static final Lock CREDENTIAL_LOCK = new ReentrantLock();
+
+	private static final int TOKEN_WAIT_DURATION_MS = 20000;
 
     private SQLServerSecurityUtility() {
         throw new UnsupportedOperationException(SQLServerException.getErrString("R_notSupported"));
@@ -340,7 +344,7 @@ class SQLServerSecurityUtility {
      * @throws SQLServerException
      */
     static SqlAuthenticationToken getManagedIdentityCredAuthToken(String resource,
-            String managedIdentityClientId) throws SQLServerException {
+            String managedIdentityClientId, long millisecondsRemaining) throws SQLServerException {
 
         if (logger.isLoggable(java.util.logging.Level.FINEST)) {
             logger.finest("Getting Managed Identity authentication token for: " + managedIdentityClientId);
@@ -379,7 +383,7 @@ class SQLServerSecurityUtility {
 
         SqlAuthenticationToken sqlFedAuthToken = null;
 
-        Optional<AccessToken> accessTokenOptional = mic.getToken(tokenRequestContext).blockOptional();
+        Optional<AccessToken> accessTokenOptional = mic.getToken(tokenRequestContext).timeout(Duration.of(Math.min(millisecondsRemaining, TOKEN_WAIT_DURATION_MS), ChronoUnit.MILLIS)).blockOptional();
 
         if (!accessTokenOptional.isPresent()) {
             throw new SQLServerException(SQLServerException.getErrString("R_ManagedIdentityTokenAcquisitionFail"),
@@ -408,7 +412,7 @@ class SQLServerSecurityUtility {
      * @throws SQLServerException
      */
     static SqlAuthenticationToken getDefaultAzureCredAuthToken(String resource,
-            String managedIdentityClientId) throws SQLServerException {
+            String managedIdentityClientId, int millisecondsRemaining) throws SQLServerException {
         String intellijKeepassPath = System.getenv(INTELLIJ_KEEPASS_PASS);
         String[] additionallyAllowedTenants = getAdditonallyAllowedTenants();
 
@@ -463,7 +467,7 @@ class SQLServerSecurityUtility {
 
         SqlAuthenticationToken sqlFedAuthToken = null;
 
-        Optional<AccessToken> accessTokenOptional = dac.getToken(tokenRequestContext).blockOptional();
+        Optional<AccessToken> accessTokenOptional = dac.getToken(tokenRequestContext).timeout(Duration.of(Math.min(millisecondsRemaining, TOKEN_WAIT_DURATION_MS), ChronoUnit.MILLIS)).blockOptional();
 
         if (!accessTokenOptional.isPresent()) {
             throw new SQLServerException(SQLServerException.getErrString("R_ManagedIdentityTokenAcquisitionFail"),
