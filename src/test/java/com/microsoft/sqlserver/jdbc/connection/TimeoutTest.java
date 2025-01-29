06f2b3b4786f8d1f923aa77bdfcfdccca6730add
Jeff Wasty <jeff.wasty@gmail.com>
2025-01-28 16:49:42 -0800

    Remove unused test

--- src/test/java/com/microsoft/sqlserver/jdbc/connection/TimeoutTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/connection/TimeoutTest.java
@@ -171,37 +171,7 @@ public class TimeoutTest extends AbstractTest {
             DriverManager.setLoginTimeout(0); // Default to 0 again
         }
     }
-
-    // Test connect retry set to 0 (disabled)
-    @Test
-    public void testConnectRetryDisable() {
-        long totalTime = 0;
-        long timerStart = System.currentTimeMillis();
-        int interval = defaultTimeout; // long interval so we can tell if there was a retry
-        long timeout = defaultTimeout * 2; // long loginTimeout to accommodate the long interval
-
-        // non existent server with long loginTimeout, should return fast if no retries at all
-        try (Connection con = PrepUtil.getConnection(
-                "jdbc:sqlserver://" + randomServer + ";transparentNetworkIPResolution=false;loginTimeout=" + timeout
-                        + ";connectRetryCount=0;connectInterval=" + interval)) {
-            fail(TestResource.getResource("R_shouldNotConnect"));
-        } catch (Exception e) {
-            totalTime = System.currentTimeMillis() - timerStart;
-
-            assertTrue(
-                    e.getMessage().matches(TestUtils.formatErrorMsg("R_tcpipConnectionFailed"))
-                            || (TestUtils.getProperty(connectionString, "msiClientId") != null && e.getMessage()
-                                    .toLowerCase().contains(TestResource.getResource("R_loginFailedMI").toLowerCase()))
-                            || ((isSqlAzure() || isSqlAzureDW()) ? e.getMessage().toLowerCase()
-                                    .contains(TestResource.getResource("R_connectTimedOut").toLowerCase()) : false),
-                    e.getMessage());
-        }
-
-        // if there was a retry then it would take at least 1 interval long, so if < interval means there were no retries
-        assertTrue(totalTime < TimeUnit.SECONDS.toMillis(interval),
-                "total time: " + totalTime + " interval: " + TimeUnit.SECONDS.toMillis(interval));
-    }
-
+    
     // Test connect retry for non-existent server with loginTimeout
     @Test
     public void testConnectRetryBadServer() {
