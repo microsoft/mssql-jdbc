38f4e8175861c0459ef6a9004c396d50739fb9dd
Jeff Wasty <jeff.wasty@gmail.com>
2025-01-29 13:23:07 -0800

    Add back missing import
    Signed-off-by: Jeff Wasty <jeff.wasty@gmail.com>

--- src/main/java/com/microsoft/sqlserver/jdbc/ISQLServerEnclaveProvider.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/ISQLServerEnclaveProvider.java
@@ -35,6 +35,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicLong;
