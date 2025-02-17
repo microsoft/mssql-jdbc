e6d626abed6b57680016140834f1586d0e6a79e1
Muskan Gupta <muskgupta@microsoft.com>
2025-02-17 22:22:25 +0530

    Removed local formatting done by default

--- src/test/java/com/microsoft/sqlserver/jdbc/resultset/ResultSetTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/resultset/ResultSetTest.java
@@ -172,8 +172,8 @@ public class ResultSetTest extends AbstractTest {
                     blob = rs.getObject(7, Blob.class);
                     try {
                         assertArrayEquals(
-                                new byte[] { 0x63, (byte) 0xC3, 0x4D, 0x6B, (byte) 0xCA, (byte) 0xD5, 0x55, (byte) 0xEB,
-                                        0x64, (byte) 0xBF, 0x7E, (byte) 0x84, (byte) 0x8D, 0x02, (byte) 0xC3, 0x76 },
+                                new byte[] {0x63, (byte) 0xC3, 0x4D, 0x6B, (byte) 0xCA, (byte) 0xD5, 0x55, (byte) 0xEB,
+                                        0x64, (byte) 0xBF, 0x7E, (byte) 0x84, (byte) 0x8D, 0x02, (byte) 0xC3, 0x76},
                                 blob.getBytes(1, 16));
                     } finally {
                         blob.free();
@@ -196,8 +196,8 @@ public class ResultSetTest extends AbstractTest {
                     }
 
                     assertArrayEquals(
-                            new byte[] { 0x63, (byte) 0xC3, 0x4D, 0x6B, (byte) 0xCA, (byte) 0xD5, 0x55, (byte) 0xEB,
-                                    0x64, (byte) 0xBF, 0x7E, (byte) 0x84, (byte) 0x8D, 0x02, (byte) 0xC3, 0x76 },
+                            new byte[] {0x63, (byte) 0xC3, 0x4D, 0x6B, (byte) 0xCA, (byte) 0xD5, 0x55, (byte) 0xEB,
+                                    0x64, (byte) 0xBF, 0x7E, (byte) 0x84, (byte) 0x8D, 0x02, (byte) 0xC3, 0x76},
                             rs.getObject(10, byte[].class));
 
                     assertEquals(java.sql.Date.valueOf("2017-05-19"), rs.getObject(11, java.sql.Date.class));
@@ -217,8 +217,7 @@ public class ResultSetTest extends AbstractTest {
                     assertEquals("2017-05-19 10:47:15.1234567 +02:00",
                             rs.getObject("col14", microsoft.sql.DateTimeOffset.class).toString());
 
-                    // BigDecimal#equals considers the number of decimal places (ResultSet returns
-                    // all digits after
+                    // BigDecimal#equals considers the number of decimal places (ResultSet returns all digits after
                     // decimal unlike CallableStatement outparams)
                     assertEquals(0, rs.getObject(15, BigDecimal.class).compareTo(new BigDecimal("0.123456789")));
                     assertEquals(0, rs.getObject("col15", BigDecimal.class).compareTo(new BigDecimal("0.123456789")));
@@ -227,13 +226,11 @@ public class ResultSetTest extends AbstractTest {
                             .compareTo(new BigDecimal("0.12345678901234567890123456789012345670")));
                     assertEquals(0, rs.getObject("col16", BigDecimal.class)
                             .compareTo(new BigDecimal("0.12345678901234567890123456789012345670")));
-
                     String expectedJsonValue = "{\"test\":\"123\"}";
                     assertEquals(expectedJsonValue, rs.getObject(17).toString());
                     assertEquals(expectedJsonValue, rs.getObject("col17").toString());
 
-                    // test null values, mostly to verify primitive wrappers do not return default
-                    // values
+                    // test null values, mostly to verify primitive wrappers do not return default values
                     assertTrue(rs.next());
                     assertNull(rs.getObject("col1", Boolean.class));
                     assertNull(rs.getObject(1, Boolean.class));
@@ -364,8 +361,7 @@ public class ResultSetTest extends AbstractTest {
             TimeZone prevTimeZone = TimeZone.getDefault();
             TimeZone.setDefault(TimeZone.getTimeZone("America/Edmonton"));
 
-            // a local date/time that does not actually exist because of Daylight Saving
-            // Time
+            // a local date/time that does not actually exist because of Daylight Saving Time
             final String testValueDate = "2018-03-11";
             final String testValueTime = "02:00:00.1234567";
             final String testValueDateTime = testValueDate + "T" + testValueTime;
@@ -398,8 +394,7 @@ public class ResultSetTest extends AbstractTest {
     }
 
     /**
-     * Tests getObject(n, java.time.OffsetDateTime.class) and getObject(n,
-     * java.time.OffsetTime.class).
+     * Tests getObject(n, java.time.OffsetDateTime.class) and getObject(n, java.time.OffsetTime.class).
      * 
      * @throws SQLException
      */
@@ -459,15 +454,13 @@ public class ResultSetTest extends AbstractTest {
     }
 
     /**
-     * Tests calling any getter on a null column should work regardless of their
-     * type.
+     * Tests calling any getter on a null column should work regardless of their type.
      * 
      * @throws SQLException
      */
     @Test
     public void testGetterOnNull() throws SQLException {
-        try (Connection con = getConnection();
-                Statement stmt = con.createStatement();
+        try (Connection con = getConnection(); Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery("select null")) {
             rs.next();
             assertEquals(null, rs.getTime(1));
@@ -482,10 +475,9 @@ public class ResultSetTest extends AbstractTest {
     @Test
     @Tag(Constants.xAzureSQLDW)
     public void testGetSetHoldability() throws SQLException {
-        int[] holdabilityOptions = { ResultSet.HOLD_CURSORS_OVER_COMMIT, ResultSet.CLOSE_CURSORS_AT_COMMIT };
+        int[] holdabilityOptions = {ResultSet.HOLD_CURSORS_OVER_COMMIT, ResultSet.CLOSE_CURSORS_AT_COMMIT};
 
-        try (Connection con = getConnection();
-                Statement stmt = con.createStatement();
+        try (Connection con = getConnection(); Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery("select null")) {
 
             int connHold = con.getHoldability();
@@ -674,8 +666,7 @@ public class ResultSetTest extends AbstractTest {
             assert (warningCount == 26);
 
             /*
-             * Testing Scenario: There are no more results when the following is true
-             * ............
+             * Testing Scenario: There are no more results when the following is true ............
              * ((stmt.getMoreResults() == false) && (stmt.getUpdateCount() == -1))
              */
             stmt.execute("INSERT INTO " + AbstractSQLGenerator.escapeIdentifier(tableName) + " values (12345)");
@@ -717,8 +708,7 @@ public class ResultSetTest extends AbstractTest {
                     .execute("select * from " + AbstractSQLGenerator.escapeIdentifier(tableName) + "; select 1/0");
             while (hasResults) {
                 ResultSet rs = stmt.getResultSet();
-                while (rs.next()) {
-                }
+                while (rs.next()) {}
                 hasResults = stmt.getMoreResults();
             }
             fail(TestResource.getResource("R_expectedFailPassed"));
