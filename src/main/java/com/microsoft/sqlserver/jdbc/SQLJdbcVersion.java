d99e8c58ff43e6fbd43b41eef1d0bf4191a7cf78
Muskan Gupta <muskgupta@microsoft.com>
2025-10-15 09:57:33 +0530

    Update PATCH version to 1

--- src/main/java/com/microsoft/sqlserver/jdbc/SQLJdbcVersion.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/SQLJdbcVersion.java
@@ -8,7 +8,7 @@ package com.microsoft.sqlserver.jdbc;
 final class SQLJdbcVersion {
     static final int MAJOR = 12;
     static final int MINOR = 2;
-    static final int PATCH = 0;
+    static final int PATCH = 1;
     static final int BUILD = 0;
     /*
      * Used to load mssql-jdbc_auth DLL.
